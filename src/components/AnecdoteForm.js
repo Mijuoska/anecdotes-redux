422d78f466ed21d1ec64d75a657ceaeef91f8f88
M.K <miika.kallasoja@gmail.com>
2020-09-27 13:59:14 +0300

    6.21

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 import { connect } from 'react-redux'
--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,16 +1,23 @@
+    let timeoutId
+
 export const setNotification = (message, timeout) => {
     return async dispatch => {
         dispatch(
             {type: 'SET',
         content: message}
         )
-        setTimeout(() => {
+        if (timeoutId) {
+            clearTimeout(timeoutId)
+        }
+         timeoutId = setTimeout(() => {
             dispatch({type: 'CLEAR', content: ''})
             
             }, timeout)
+
     }
 } 
 
+// Notification is set --> timeout is set for clear message 
 
 const notificationReducer = (state = '', action) => {
     switch(action.type) {
